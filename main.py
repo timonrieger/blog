53542caf9fd847a53c4db9d856623a45529a71fe
timonrieger <github.com.kindle922@passmail.net>
2025-01-17 21:51:13 +0100

    chore: sqlalchemy untrack modifications

--- main.py
+++ main.py
@@ -112,7 +112,8 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"#os.environ.get("DB_URI", "sqlite:///blog.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///blog.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
