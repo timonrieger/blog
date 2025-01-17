c92654a4469d3305ff673d3d74c5ad8f323f1136
timonrieger <github.com.kindle922@passmail.net>
2025-01-17 20:36:32 +0100

    refactor: group utils by usecase

--- src/utils.py
+++ src/utils.py
@@ -10,10 +10,42 @@ from datetime import datetime, timezone, timedelta
 import humanize
 
 
-def get_time(timezone_offset=TIMEZONE_OFFSET):
-    """Return current time for the specified timezone."""
+# JINJA FILTER
+def from_json(json_string):
+    """Deserialize (a str, bytes or bytearray instance containing a JSON document) to a Python object. Used as and"""
+    return json.loads(json_string)
+
+_LOCAL_MAPPING = {
+    "de": "de_DE",
+}
+
+def humanize_time(date, language=LANGUAGE, timezone_offset=TIMEZONE_OFFSET):
+    '''Humanizes a datetime.timedelta object, e.g. one hour ago.'''
+    lang = _LOCAL_MAPPING.get(language, None)
+    humanize.i18n.activate(lang)
     tzinfo = timezone(timedelta(hours=timezone_offset))
-    return datetime.now(tz=tzinfo)
+    input_date = date.replace(tzinfo=timezone.utc).astimezone(tzinfo)
+    current = datetime.now(tz=tzinfo)
+    print(type(humanize.naturaltime(current - input_date)))
+    return humanize.naturaltime(current - input_date)
+
+# FORMS UTILS
+def validate_tags_format(form, tags):
+    """
+    Validates that the tags are comma-separated and do not include any whitespaces.
+
+    :param tags: A string containing comma-separated tags
+    :return: A list of valid tags if the format is correct, otherwise raises a ValueError
+    """
+    if form.tags.data == "":
+        return []
+
+    tags_list = [tag.strip() for tag in form.tags.data.split(",")]
+
+    if any(not tag for tag in tags_list):
+        raise ValueError("Tags must be separated by commas and cannot be empty.")
+
+    return tags_list
 
 
 def suggest_img_url(directory):
@@ -39,6 +71,65 @@ def suggest_img_url(directory):
         suggestion = f"{directory}{incremented_number}{ext}"
     finally:
         return lazy_gettext("Suggestion: %(suggestion)s", suggestion=suggestion)
+
+
+
+# DB FILTER UTILS
+def filter_posts_by_tag(tag, current_user, posts):
+    """Filter by tag and draft posts if the user is an admin"""
+    if tag.lower() == "draft" and current_user.is_authenticated and current_user.admin:
+        return [post for post in posts if post.is_draft]
+    else:
+        return [post for post in posts if tag in json.loads(post.tags)]
+
+
+def filter_posts_by_year(year, posts):
+    """Filter by the year the post was created."""
+    return [post for post in posts if year == str(post.create_date.strftime("%Y"))]
+
+
+def hide_drafts(current_user, SUPER_ID, posts):
+    """Filter out drafts if the user is not the author or the super admin"""
+    if current_user.is_authenticated:
+        return [
+            post
+            for post in posts
+            if not post.is_draft
+            or (post.is_draft and post.author_id == current_user.id)
+            or current_user.id == SUPER_ID
+        ]
+    else:
+        return [post for post in posts if not post.is_draft]
+
+
+def filter_posts_by_author(author, user_model, posts):
+    """Filter by the post author."""
+    author_id = user_model.query.filter_by(username=author).first().id
+    return [post for post in posts if author_id == post.author_id]
+
+
+# MISCELLANEOUS
+def get_tags_description(tags):
+    """Add description to tag field."""
+    if tags and tags != [""]:
+        suggestion = ", ".join(tags)
+    else:
+        suggestion = "nothing found (add tags first, e.g. travel, food, books)"
+    return lazy_gettext("Suggestion: %(suggestion)s", suggestion=suggestion)
+
+
+def get_unique_tags(post_model):
+    """Sorted tags by occurence."""
+    all_tags = [json.loads(post.tags) for post in post_model.query.all()]
+    unique_tags = set(tag for tags in all_tags for tag in tags)
+    tag_counts = Counter(tag for tags in all_tags for tag in tags)
+    return sorted(unique_tags, key=lambda tag: tag_counts[tag], reverse=True)
+
+
+def get_time(timezone_offset=TIMEZONE_OFFSET):
+    """Return current time for the specified timezone."""
+    tzinfo = timezone(timedelta(hours=timezone_offset))
+    return datetime.now(tz=tzinfo)
 
 
 def parse_title(raw_title):
@@ -83,88 +174,3 @@ def calculate_reading_time(text, words_per_minute=200):
     reading_time_minutes = total_words / words_per_minute
     minutes = int(reading_time_minutes)
     return minutes
-
-
-def from_json(json_string):
-    """Deserialize (a str, bytes or bytearray instance containing a JSON document) to a Python object. Used as and"""
-    return json.loads(json_string)
-
-_LOCAL_MAPPING = {
-    "de": "de_DE",
-}
-
-def humanize_time(date, language=LANGUAGE, timezone_offset=TIMEZONE_OFFSET):
-    lang = _LOCAL_MAPPING.get(language, None)
-    humanize.i18n.activate(lang)
-    tzinfo = timezone(timedelta(hours=timezone_offset))
-    input_date = date.replace(tzinfo=timezone.utc).astimezone(tzinfo)
-    current = datetime.now(tz=tzinfo)
-    return humanize.naturaltime(current - input_date)
-
-
-def validate_tags_format(form, tags):
-    """
-    Validates that the tags are comma-separated and do not include any whitespaces.
-
-    :param tags: A string containing comma-separated tags
-    :return: A list of valid tags if the format is correct, otherwise raises a ValueError
-    """
-    if form.tags.data == "":
-        return []
-
-    tags_list = [tag.strip() for tag in form.tags.data.split(",")]
-
-    if any(not tag for tag in tags_list):
-        raise ValueError("Tags must be separated by commas and cannot be empty.")
-
-    return tags_list
-
-
-def get_tags_description(tags):
-    """Add description to tag field."""
-    if tags and tags != [""]:
-        suggestion = ", ".join(tags)
-    else:
-        suggestion = "nothing found (add tags first, e.g. travel, food, books)"
-    return lazy_gettext("Suggestion: %(suggestion)s", suggestion=suggestion)
-
-
-def get_unique_tags(post_model):
-    """Sorted tags by occurence."""
-    all_tags = [json.loads(post.tags) for post in post_model.query.all()]
-    unique_tags = set(tag for tags in all_tags for tag in tags)
-    tag_counts = Counter(tag for tags in all_tags for tag in tags)
-    return sorted(unique_tags, key=lambda tag: tag_counts[tag], reverse=True)
-
-
-def filter_posts_by_tag(tag, current_user, posts):
-    """Filter by tag and draft posts if the user is an admin"""
-    if tag.lower() == "draft" and current_user.is_authenticated and current_user.admin:
-        return [post for post in posts if post.is_draft]
-    else:
-        return [post for post in posts if tag in json.loads(post.tags)]
-
-
-def filter_posts_by_year(year, posts):
-    """Filter by the year the post was created."""
-    return [post for post in posts if year == str(post.create_date.strftime("%Y"))]
-
-
-def hide_drafts(current_user, SUPER_ID, posts):
-    """Filter out drafts if the user is not the author or the super admin"""
-    if current_user.is_authenticated:
-        return [
-            post
-            for post in posts
-            if not post.is_draft
-            or (post.is_draft and post.author_id == current_user.id)
-            or current_user.id == SUPER_ID
-        ]
-    else:
-        return [post for post in posts if not post.is_draft]
-
-
-def filter_posts_by_author(author, user_model, posts):
-    """Filter by the post author."""
-    author_id = user_model.query.filter_by(username=author).first().id
-    return [post for post in posts if author_id == post.author_id]
